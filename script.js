9b65156226efab42fa5db28e51dee613a59a746f
HyuSie <165596009+HyuSie@users.noreply.github.com>
2025-03-02 19:06:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById("interactiveButton").addEventListener("click", function () {
     document.getElementById("message").style.display = "block";
 });
+
+document.getElementById("themeToggle").addEventListener("click", function () {
+    let body = document.body;
+    body.classList.toggle("theme1");
+    body.classList.toggle("theme2");
+});
