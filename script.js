de1db3d1912a7cc6bfcdb7001cddeaa0e414deb5
HyuSie <165596009+HyuSie@users.noreply.github.com>
2025-03-02 17:52:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-/* script.js */
 document.addEventListener("DOMContentLoaded", function() {
   const button = document.getElementById("interactiveButton");
+  const themeButton = document.getElementById("themeToggle");
+
   if (button) {
     button.addEventListener("click", function() {
       const message = document.getElementById("message");
-      // Переключаем отображение сообщения при клике
-      if (message.style.display === "none" || message.style.display === "") {
-        message.style.display = "block";
-      } else {
-        message.style.display = "none";
-      }
+      message.style.display = (message.style.display === "none" || message.style.display === "") ? "block" : "none";
+    });
+  }
+
+  if (themeButton) {
+    themeButton.addEventListener("click", function() {
+      document.body.classList.toggle("theme1");
+      document.body.classList.toggle("theme2");
     });
   }
 });
