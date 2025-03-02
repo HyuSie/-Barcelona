26f3ba5c1ce745db534a4ad71c72bfd157ee15d0
HyuSie <165596009+HyuSie@users.noreply.github.com>
2025-03-02 19:21:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-document.getElementById("interactiveButton").addEventListener("click", function () {
-    document.getElementById("message").style.display = "block";
-});
-
-document.getElementById("themeToggle").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
+    let themeButton = document.getElementById("themeToggle");
     let body = document.body;
-    body.classList.toggle("theme1");
-    body.classList.toggle("theme2");
+
+    // Проверяем, какая тема была сохранена в локальном хранилище
+    if (localStorage.getItem("theme") === "theme2") {
+        body.classList.remove("theme1");
+        body.classList.add("theme2");
+    }
+
+    themeButton.addEventListener("click", function () {
+        body.classList.toggle("theme1");
+        body.classList.toggle("theme2");
+
+        // Сохраняем выбор пользователя в локальное хранилище
+        if (body.classList.contains("theme2")) {
+            localStorage.setItem("theme", "theme2");
+        } else {
+            localStorage.setItem("theme", "theme1");
+        }
+    });
 });
